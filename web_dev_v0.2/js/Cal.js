be128f4d3452987715292f1da12dfd429b67e65b
runtime-terror-028 <runtime.terror.028@gmail.com>
2022-12-24 18:12:05 +0530

    Calculater completed

---
+++ web_dev_v0.2/js/Cal.js
@@ -0,0 +1,3 @@
+function input(abc){document.form.textview.value =document.form.textview.value+abc;}
+function AC(){document.form.textview.value="";}
+function result(){document.form.textview.value=eval(document.form.textview.value)}
